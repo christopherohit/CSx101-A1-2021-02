f0840d93287c4a8ce8da6dd93569d049d8788cdc
christopherohit <55018999+christopherohit@users.noreply.github.com>
2021-09-02 09:26:53 +0300

    Cập Nhập bài tập buổi một

---
+++ Assignment/bai1.py
@@ -0,0 +1,7 @@
+a = int(input())
+b = int(input())
+
+min = ((a + b)- abs(a-b))/2
+max = ((a + b)+ abs(a-b))/2
+print (max)
+print(min)
---
+++ Assignment/bai1_lan2.py
@@ -0,0 +1,7 @@
+#Transfer to C and K
+
+C = float(input())
+Pc = int((C-32) * (5/9))
+Pk = float(273.15 + Pc)
+print(Pc)
+print(Pk)
