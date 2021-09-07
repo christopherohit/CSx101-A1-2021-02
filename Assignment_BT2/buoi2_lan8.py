463db20c444fa442f2b2463570b5d616fafb9017
christopherohit <55018999+christopherohit@users.noreply.github.com>
2021-09-08 02:05:38 +0300

    Complete this Problem

--- Assignment_BT2/buoi2_lan8.py
+++ Assignment_BT2/buoi2_lan8.py
@@ -31,33 +31,14 @@ Giá trị 1 nếu 4 biến trên thoã mãn
 
 Tr, Xd, Cr, Rk = [int(x) for x in input().split()]
 
-if (Tr == 1):
-    if(Xd == 1 or Rk == 1):
-        if (Cr == 1):
-            print(1)
-        else:
-            print(0)
-    elif Xd == 0:
-        print(0)
-    elif Rk == 1 or Rk == 0:
-        print(1)
+if (Tr == 1 and Xd == 0):
+    print(0)
+elif (Xd ==1  and Cr == 0 and Rk == 0):
+    print(0)
+elif Xd == 1 and Rk == 1 and Cr == 0:
+    print(0)
+elif Xd ==0 and Rk == 0 and Cr == 1:
+    print(0)
+
 else:
-    if (Xd == 1):
-        print(0)
-    elif Rk == 1:
-        if Cr == 1:
-            print(1)
-        else:
-            print(1)
-    elif Xd == 0 and Rk == 1:
-        if Cr == 1:
-            print(1)
-        else:
-            print(0)
-    elif Xd == 0 and Rk == 0:
-        if Cr == 1 :
-            print(0)
-        else:
-            print(1)
-    else:
-        print(1)
+    print(1)
